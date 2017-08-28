7d21891f998a8d527f1758aa0512646224dca7ce
Luis Gustavo Miki <g.miki@live.com>
2017-08-28 05:52:23 -0300

    Progress on ninja.miki.facebook.js

--- procs/ninja.miki.facebook.js
+++ procs/ninja.miki.facebook.js
@@ -1,3 +1,6 @@
+const async = require('async');
+const FB = require('fb');
+const URL = require('url');
 
 module.exports = function(agenda) {
 	agenda.define('ninja.miki.facebook.fetch_timeline', function(job, done) {
@@ -5,4 +8,30 @@ module.exports = function(agenda) {
 		console.log("HELLO FROM FETCH TIMELINE");
 		done();
 	});	
+}
+
+function processFacebookFeed (feed, params, cb) {
+  var totalResults, done;
+    totalResults = []; // progressively store results here
+    done = false; // will be set to true to terminate loop
+
+  async.doUntil(function(callback) {
+    // body of the loop
+    FB.napi(feed, params, function(err, result) {
+      if (err) return callback(err);
+        totalResults = totalResults.concat(result.data);
+        // console.log("PAGING CYCLE");
+        if (result.data.length === 0 || !result.paging.next) {
+          done = true;
+        } else {
+          params = URL.parse(result.paging.next, true).query;
+        }
+        callback();
+      }); // FB.napi
+    }, function() {
+      // test for loop termination
+      return done;
+    }, function (err) {
+      cb(err, totalResults);
+    }); // async.doUntil
 }
